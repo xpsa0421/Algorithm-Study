48b091eec1e8af8551f231fceff1882c4a49c138
Sooa <71711432+xpsa0421@users.noreply.github.com>
2023-08-17 23:47:53 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << "\n";
+	}
+}
