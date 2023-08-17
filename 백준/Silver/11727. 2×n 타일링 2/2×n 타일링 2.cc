fb32397f859b04fda14aa9269f9b22c6cd1e3b70
Sooa <71711432+xpsa0421@users.noreply.github.com>
2023-08-17 23:42:19 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int methods[1001];
+	methods[1] = 1;
+	methods[2] = 3;
+
+	for (int i = 3; i < n + 1; i++)
+	{
+		methods[i] = (methods[i - 1] + methods[i - 2] + methods[i - 2]) % 10007;
+	}
+
+	cout << methods[n] % 10007;
+}
