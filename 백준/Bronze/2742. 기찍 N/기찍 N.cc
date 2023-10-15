d93e0f69f8d1da9cb38a3cf5c9292b06ea599ab2
Sooa <71711432+xpsa0421@users.noreply.github.com>
2023-10-15 21:05:21 +0800

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+
+	int n = 0;
+	cin >> n;
+	
+	for (int i = n; i > 0; i--)
+	{
+		cout << i << "\n";
+	}
+}
