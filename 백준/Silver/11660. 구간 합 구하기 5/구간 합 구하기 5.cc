998bb1d0c2f2cd16631065d2f66ad50e45656157
Sooa <71711432+xpsa0421@users.noreply.github.com>
2024-07-24 13:06:39 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 132 ms, Memory: 6264 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -2,8 +2,6 @@
 #include <vector>
 using namespace std;
 
-int Solve(const vector<vector<int>>& vec, int x1, int y1, int x2, int y2);
-
 int main()
 {
 	ios_base::sync_with_stdio(0);
@@ -12,47 +10,22 @@ int main()
 	int n,m;
 	cin >> n >> m;
 
-	// psum[y,x]는 [0][0]에서 [y][x]까지의 부분합
 	vector<vector<int>> psum(n + 1, vector<int>(n + 1, 0));
 	int cur;
 
-	for (int y = 0; y <= n; y++)
+	for (int x = 1; x <= n; x++)
 	{
-		for (int x = 0; x <= n; x++)
+		for (int y = 1; y <= n; y++)
 		{
-			if (x == 0 || y == 0)
-			{
-				psum[y][x] = 0;
-			}
-			else
-			{
-				cin >> cur;
-				psum[y][x] = psum[y - 1][x]
-					+ (psum[y][x - 1] - psum[y - 1][x - 1])
-					+ cur;
-			}
+			cin >> cur;
+			psum[x][y] = psum[x - 1][y] + (psum[x][y - 1] - psum[x - 1][y - 1]) + cur;
 		}
 	}
 
-	vector<int> ans;
 	int x1, y1, x2, y2;
 	while (m--)
 	{
 		cin >> x1 >> y1 >> x2 >> y2;
-		ans.push_back(Solve(psum, y1, x1, y2, x2));
+		cout << psum[x2][y2] - psum[x1 - 1][y2] - psum[x2][y1 - 1] + psum[x1 - 1][y1 - 1] << '\n';
 	}
-
-	for (int i = 0; i < ans.size(); i++)
-	{
-		cout << ans[i] << "\n";
-	}
-}
-
-int Solve(const vector<vector<int>>& psum, int x1, int y1, int x2, int y2)
-{
-	int ret = psum[y2][x2]
-		- psum[y1 - 1][x2]
-		- psum[y2][x1 - 1]
-		+ psum[y1 - 1][x1 - 1];
-	return ret;
 }
