cc463ab8243138bbe6815df956c41c04ed19e50e
Sooa <71711432+xpsa0421@users.noreply.github.com>
2024-10-30 23:22:33 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -4,6 +4,24 @@
 #include <algorithm>
 using namespace std;
 
+int p[201];
+
+int find(int a)
+{
+    if (p[a] < 0) return a;
+    return p[a] = find(p[a]);
+}
+
+bool merge(int a, int b)
+{
+    a = find(a), b = find(b);
+    if (a == b) return false;
+    if (p[a] == p[b]) p[a]--;
+    if (p[a] < p[b]) p[b] = a;
+    else p[a] = b;
+    return true;
+}
+
 int main() 
 {
     ios_base::sync_with_stdio(0);
@@ -12,14 +30,14 @@ int main()
     int n; cin >> n;
     int m; cin >> m;
 
-    vector<vector<int>> dist(n + 1, vector<int>(n + 1,201));
+    fill(p, p + 201, -1);
+
     for (int i = 1; i <= n; i++)
     {
-        dist[i][i] = 0;
         for (int j = 1; j <= n; j++)
         {
             int k; cin >> k;
-            if (k == 1) dist[i][j] = 1;
+            if (k == 1) merge(i, j);
         }
     }
 
@@ -27,27 +45,12 @@ int main()
     for (int i = 0; i < m; i++)
         cin >> path[i];
 
-    for (int k = 1; k <= n; k++)
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= n; j++)
-            {
-                if (i == j) continue;
-
-                // 경유지를 통해서 갈 수 없는 경우
-                if (dist[i][k] == 201 || dist[k][j] == 201) continue;
-
-                // 경유지를 통하는 게 더 가까운 경우
-                dist[i][j] = min(dist[i][k] + dist[k][j], dist[i][j]);
-            }
-        }
-    }
-
     bool isValid = true;
     for (int i = 1; i < m; i++)
     {
-        if (dist[path[i - 1]][path[i]] == 201)
+        int a = path[i - 1];
+        int b = path[i];
+        if (merge(a,b))
         {
             isValid = false;
             break;
